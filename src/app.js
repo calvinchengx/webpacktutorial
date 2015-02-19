c8518cf12348de84c6aeb3288321e5b9a77256c4
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-19 12:22:31 +0800

    Example usage for asynchronously required module

--- src/app.js
+++ src/app.js
@@ -8,3 +8,8 @@ require('hash-change').on('change', function(hash) {
 
 // require additional css
 require('./css/style.css');
+
+// example usage for asynchronously required module
+require(['./js/exampleasync.js'], function() {
+  console.log('loaded example async'); 
+});
